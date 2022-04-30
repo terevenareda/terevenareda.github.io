8fd9899552f836072d799cb89b9925de679c68a4
terevenareda <104698750+terevenareda@users.noreply.github.com>
2022-05-01 00:02:19 +0200

    terevena - initial version

---
+++ css/project.js
@@ -0,0 +1,71 @@
+var content = document.getElementById("box");
+function added(){
+    window.alert("item added to cart");
+};
+
+var first = document.querySelector("#add");
+first.addEventListener('click',added);
+
+var second = document.querySelector("#add1");
+second.addEventListener('click',added);
+
+var third = document.querySelector("#add2");
+third.addEventListener('click',added);
+
+var four = document.querySelector("#add3");
+four.addEventListener('click',added);
+
+var five = document.querySelector("#add4");
+five.addEventListener('click',added);
+
+var six = document.querySelector("#add5");
+six.addEventListener('click',added);
+
+var seven = document.querySelector("#add6");
+seven.addEventListener('click',added);
+
+var eight = document.querySelector("#add7");
+eight.addEventListener('click',added);
+
+var nine = document.querySelector("#add8");
+nine.addEventListener('click',added);
+
+var ten = document.querySelector("#add9");
+ten.addEventListener('click',added);
+
+var eleven = document.querySelector("#add10");
+eleven.addEventListener('click',added);
+
+var twelve = document.querySelector("#add11");
+twelve.addEventListener('click',added);
+
+var thir = document.querySelector("#add12");
+thir.addEventListener('click',added);
+
+var fourt = document.querySelector("#add13");
+fourt.addEventListener('click',added);
+
+var fif = document.querySelector("#add14");
+fif.addEventListener('click',added);
+
+var first = document.querySelector("#add15");
+first.addEventListener('click',added);
+
+var sixt = document.querySelector("#add16");
+sixt.addEventListener('click',added);
+
+var sevnt = document.querySelector("#add17");
+sevnt.addEventListener('click',added);
+
+var eightt = document.querySelector("#add18");
+eightt.addEventListener('click',added);
+
+var ninet = document.querySelector("#add19");
+ninet.addEventListener('click',added);
+
+var twent = document.querySelector("#add20");
+twent.addEventListener('click',added);
+
+
+
+
